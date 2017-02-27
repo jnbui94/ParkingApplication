dc17348b712fa34b68d4a75491062dc24de8493d
jnbui <jnbui94@gmail.com>
2017-02-27 12:19:16 -0800

    Implemented ParkingLots tab

---
+++ ParkingApplication/src/data/ParkingLotDB.java
@@ -0,0 +1,117 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+import parking.ParkingLots;
+
+/**
+ * This class access to ParkingLot tables
+ * @author John Bui
+ *
+ */
+public class ParkingLotDB {
+	/**
+	 * static variable for connection.
+	 */
+	private static Connection mConnection;
+	/**
+	 * static variable for parking.
+	 */
+	private static List<ParkingLots>  mParkingList;
+
+	/**
+	 * Retrieves all parking lot from parking table.
+	 * 
+	 * @return list of ParkingLot
+	 * @throws SQLException
+	 */
+	public static List<ParkingLots> getParkingLots() throws SQLException {
+		if (mConnection == null) {
+			mConnection = DataConnection.getConnection();
+		}
+		Statement stmt = null;
+		String query = "select * " + "from ParkingLot";
+
+		mParkingList = new ArrayList<ParkingLots>();
+		try {
+			stmt = mConnection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String name = rs.getString("name");
+				String location = rs.getString("location");
+				int capacity = rs.getInt("capacity");
+				int floor = rs.getInt("numOfFloors");
+				ParkingLots parkingLots = null;
+				parkingLots = new ParkingLots(name,location,capacity,floor);
+				mParkingList.add(parkingLots);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e);
+			
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+		return mParkingList;
+	}
+	/**
+	 * Returns all parking lots that contain the search keyword in the name or
+	 * @param name name of parking lots
+	 * @return list of items that match.
+	 * @throws SQLException
+	 */
+	public List<ParkingLots> getParkingLots(String name) throws SQLException {
+		List<ParkingLots> filterList = new ArrayList<ParkingLots>();
+		if (mParkingList == null) {
+			getParkingLots();
+		}
+		name = name.toLowerCase();
+		for (ParkingLots parkingLots : mParkingList) {
+			if (parkingLots.getName().toLowerCase().contains(name)) {
+				filterList.add(parkingLots);
+			}
+		}
+		return filterList;
+	}
+	/**
+	 * Adds a new parking lots to parking lots table. 
+	 * @param member
+	 * @return Returns "Added Item Successfully" or pop-up a Joptionpane for warning.
+	 */
+	public static String addParkingLot(ParkingLots lots) {
+		String sql = "insert into ParkingLot(name, location, capacity, numOfFloors) values "
+				+ "(?, ?, ?, ?); ";
+
+		if (mConnection == null) {
+			try {
+				mConnection = DataConnection.getConnection();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = mConnection.prepareStatement(sql);
+			preparedStatement.setString(1, lots.getName());
+			preparedStatement.setString(2, lots.getLocation());
+			preparedStatement.setInt(3, lots.getCapacity());
+			preparedStatement.setInt(4, lots.getFloor());
+			preparedStatement.executeUpdate();
+			return "Added Member Successfully";
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+			return "fail";
+		}
+	}
+
+}
--- ParkingApplication/src/ui/ParkingLotGUI.java
+++ ParkingApplication/src/ui/ParkingLotGUI.java
@@ -11,19 +11,24 @@ import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
+import StaffMember.StaffMember;
+import data.MemberDB;
+import data.ParkingLotDB;
+import parking.ParkingLots;
+
 
 /**
  * A Panel that contains all the Item related functionality to 
  * list the items, search the items, add a new item, modify values within the item.
- * @author mabraham
+ * @author Loc Bui.
  *
  */
 
-public class ParkingLotGUI extends JPanel {
+public class ParkingLotGUI extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 1779520078061383929L;
 	private JButton btnList, btnAdd;
 	private JPanel pnlButtons, pnlContent;
-//	private List<Item> mList;
+	private List<ParkingLots> mList;
 
 	private String[] mItemColumnNames = { "name", "location", "capacity",
 			"numOfFloors" };
@@ -35,7 +40,7 @@ public class ParkingLotGUI extends JPanel {
 	private JPanel pnlAdd;
 	private JLabel[] txfLabel = new JLabel[4];
 	private JTextField[] txfField = new JTextField[4];
-	private JButton btnAddItem;
+	private JButton btnAddParking;
 
 	/**
 	 * Use this for Item administration. Add components that contain the list,
@@ -43,7 +48,7 @@ public class ParkingLotGUI extends JPanel {
 	 */
 	public ParkingLotGUI() {
 		setLayout(new BorderLayout());
-//		mList = getData(null);
+		mList = getData();
 		createComponents();
 		setVisible(true);
 		setSize(500, 500);
@@ -56,26 +61,24 @@ public class ParkingLotGUI extends JPanel {
 	 * 
 	 * @return
 	 */
-//	private List<Item> getData(String searchKey) {
-//
-//		if (searchKey != null)
-//			mList = ItemCollection.search(searchKey);
-//		else
-//			mList = ItemCollection.getItems();
-//
-//		if (mList != null) {
-//			mData = new Object[mList.size()][mItemColumnNames.length];
-//			for (int i = 0; i < mList.size(); i++) {
-//				mData[i][0] = mList.get(i).getName();
-//				mData[i][1] = mList.get(i).getDescription();
-//				mData[i][2] = mList.get(i).getPrice();
-//				mData[i][3] = mList.get(i).getCondition();
-//				mData[i][4] = mList.get(i).getItemCategory().getCategory();
-//			}
-//		}
-//
-//		return mList;
-//	}
+	private List<ParkingLots> getData() {
+		try {
+			mList = ParkingLotDB.getParkingLots();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+		if (mList != null) {
+			mData = new Object[mList.size()][mItemColumnNames.length];
+			for (int i = 0; i < mList.size(); i++) {
+				mData[i][0] = mList.get(i).getName();
+				mData[i][1] = mList.get(i).getLocation();
+				mData[i][2] = mList.get(i).getCapacity();
+				mData[i][3] = mList.get(i).getFloor();
+			}
+		}
+		return mList;
+		
+	}
 
 	/*
 	 * Create the three panels to add to this GUI. One for list, one for search,
@@ -85,31 +88,30 @@ public class ParkingLotGUI extends JPanel {
 		// A button panel at the top for list, search, add
 		pnlButtons = new JPanel();
 		btnList = new JButton("Parking Lot List");
-
+		btnList.addActionListener(this);
 
 		btnAdd = new JButton("Add Parking Lot");
-
+		btnAdd.addActionListener(this);
+		
 		pnlButtons.add(btnList);
 		pnlButtons.add(btnAdd);
 		add(pnlButtons, BorderLayout.NORTH);
 
-//		addListPanel();
-		
-		//I create a new method for the add panel in order to refresh the category ComboBox.
-		// @author Loc Bui
-//		addPanel();
-
-//		add(pnlContent, BorderLayout.CENTER);
+		addListPanel();
+		addPanel();
+		add(pnlContent, BorderLayout.CENTER);
 
 	}
-	
-//	public void addListPanel() {
-//		// List Panel
-//		pnlContent = new JPanel();
-//		table = new JTable(mData, mItemColumnNames);
-//		scrollPane = new JScrollPane(table);
-//		pnlContent.add(scrollPane);
-//	}
+	/**
+	 * This method add listPanel of parking lot.
+	 */
+	public void addListPanel() {
+		// List Panel
+		pnlContent = new JPanel();
+		table = new JTable(mData, mItemColumnNames);
+		scrollPane = new JScrollPane(table);
+		pnlContent.add(scrollPane);
+	}
 	
 	/**
 	 * This method create the add panel.
@@ -118,7 +120,7 @@ public class ParkingLotGUI extends JPanel {
 	public void addPanel() {
 		// Add Panel
 		pnlAdd = new JPanel();
-		pnlAdd.setLayout(new GridLayout(4, 0));
+		pnlAdd.setLayout(new GridLayout(6, 0));
 		String labelNames[] = { "Enter Name:", "Enter Location: ",
 				"Enter Capacity: ", "Enter Number Of Floors: " };
 		for (int i = 0; i < labelNames.length; i++) {
@@ -133,80 +135,80 @@ public class ParkingLotGUI extends JPanel {
 
 
 		JPanel panel = new JPanel();
-		btnAddItem = new JButton("Add");
-		panel.add(btnAddItem);
+		btnAddParking = new JButton("Add");
+		btnAddParking.addActionListener(this);
+		panel.add(btnAddParking);
 		pnlAdd.add(panel);
 	}
-
-//	/**
-//	 * Allows to add an Item. Only name is required.
-//	 */
-//	private void performAddItem() {
-//
-//		String name = txfField[0].getText();
-//		if (name.length() == 0) {
-//			JOptionPane.showMessageDialog(null, "Enter an item name");
-//			txfField[0].setFocusable(true);
-//			return;
-//		}
-//		String desc = txfField[1].getText();
-//		if (desc.length() == 0) {
-//			desc = null;
-//		}
-//		String priceStr = txfField[2].getText();
-//		double price = 0.0;
-//		if (priceStr.length() != 0) {
-//			try {
-//				price = Double.parseDouble(priceStr);
-//			} catch (NumberFormatException e) {
-//				JOptionPane.showMessageDialog(null, "Enter price as decimal");
-//				txfField[2].setText("");
-//				txfField[2].setFocusable(true);
-//				return;
-//			}
-//		}
-//		String condition = txfField[3].getText();
-//		if (condition.length() == 0) {
-//			condition = null;
-//		}
-//		String category = (String) cmbCategories.getSelectedItem();
-//		Item item;
-//		if (desc == null) {
-//			item = new Item(name, new ItemCategory(category));
-//		} else {
-//			item = new Item(name, desc, price, condition, new ItemCategory(
-//					category));
-//		}
-//		String message = "Item add failed";
-//		if (ItemCollection.add(item)) {
-//			message = "Item added";
-//		}
-//		JOptionPane.showMessageDialog(null, message);
-//
-//		// Clear all text fields.
-//		for (int i = 0; i < txfField.length; i++) {
-//			if (txfField[i].getText().length() != 0) {
-//				txfField[i].setText("");
-//			}
-//		}
-//	}
-
-//	/**
-//	 * Listen to the cell changes on the table. 
-//	 */
-//	@Override
-//	public void tableChanged(TableModelEvent e) {
-//		int row = e.getFirstRow();
-//		int column = e.getColumn();
-//		TableModel model = (TableModel) e.getSource();
-//		String columnName = model.getColumnName(column);
-//		Object data = model.getValueAt(row, column);
-//		if (data != null && ((String) data).length() != 0) {
-//			Item item = mList.get(row);
-//			if (!ItemCollection.update(item, columnName, data)) {
-//				JOptionPane.showMessageDialog(null, "Update failed");
-//			}
-//		}
-//	}
-
+	/**
+	 * Action perform response when a button is clicked.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent thEvent) {
+		if(thEvent.getSource() == btnAdd) {
+			pnlContent.removeAll();
+			addPanel();
+			pnlContent.add(pnlAdd);
+			pnlContent.revalidate();
+			this.repaint();
+		} else if (thEvent.getSource() == btnAddParking) {
+			performAddParking();
+		} else if (thEvent.getSource() == btnList) {
+			mList = getData();
+			pnlContent.removeAll();
+			table = new JTable(mData,mItemColumnNames);
+			scrollPane = new JScrollPane(table);
+			pnlContent.add(scrollPane);
+			pnlContent.revalidate();
+			pnlContent.setVisible(true);
+			this.repaint();
+		}
+		
+	}
+	/*
+	 * Perform adding a parking lot.
+	 */
+	private void performAddParking() {
+		String name = txfField[0].getText();
+		if (name.length() == 0) {
+			JOptionPane.showMessageDialog(null, "Enter a parking Lot name");
+			txfField[0].setFocusable(true);
+			return;
+		}
+		
+		String location = txfField[1].getText();
+		if (location.length() == 0) {
+			JOptionPane.showMessageDialog(null, "Enter a location");
+			txfField[1].setFocusable(true);
+			return;
+		}
+		
+		String capacitystr =  txfField[2].getText();
+		int capacity = Integer.valueOf(capacitystr);
+		if (capacity <= 0) {
+			JOptionPane.showMessageDialog(null, "Enter a valid capacity");
+			txfField[2].setFocusable(true);
+			return;
+		}
+		
+		String floorstr = txfField[3].getText();
+			int floor = Integer.valueOf(floorstr);
+		if (floor < 0) {
+			JOptionPane.showMessageDialog(null, "Enter a valid floor number");
+			txfField[3].setFocusable(true);
+			return;
+		}
+		//Create parking object and add to database.
+		ParkingLots parking;
+		parking = new ParkingLots(name,location, capacity, floor);
+		if (ParkingLotDB.addParkingLot(parking).equals("Added Member Successfully")) {
+			JOptionPane.showMessageDialog(null, "Successfully added");
+		}
+		// Clear all text fields.
+		for (int i = 0; i < txfField.length; i++) {
+			if (txfField[i].getText().length() != 0) {
+				txfField[i].setText("");
+			}
+		}
+	}
 }
